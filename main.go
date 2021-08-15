db3b4b9311ac1db19256e043c613dad1fd8c3d25
alfonmga <alfonmga@icloud.com>
2021-08-15 10:44:14 +0200

    Add comments to global variables

--- main.go
+++ main.go
@@ -4,7 +4,10 @@ import (
 	"github.com/alfonmga/safehodl/cmd"
 )
 
+// PinCode access pin code for secure access to SafeHODL.
 var PinCode string
+
+// Secret32BytesKeyAES 32 bytes AES secret key for .safehodl data file encryption.
 var Secret32BytesKeyAES string
 
 func main() {
